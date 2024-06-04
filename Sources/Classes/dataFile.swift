1da0a5deebe23ee103ff25e08274edf7420238e4
mobileexpert1 <appsdev096@gmail.com>
2024-06-04 12:10:11 +0530

    new update

---
+++ Sources/Classes/dataFile.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vishal on 04/06/24.
+//
+
+import Foundation
+import iPass2_0NativeiOS
+
+public class MainDataBase{
+    
+      func getScanCompletionResult(result: String, error: String) {
+            print(Date())
+            print("data fetched success")
+            print("Result-->", result)
+            print("Error-->", error)
+            
+        }
+    }
+  DataBaseDownloading.initialization(completion:{
+        progres, status, error in
+        print(progres,status, error)
+
+
+    })
+public func startScanningProcess() {
+ //   iPassSDKManger.delegate = self
+//    iPassSDKManger.startScanningProcess(userEmail: "", flowId: 10015, socialMediaEmail :"appsdev096@gmail.com", phoneNumber : "+919781986132", controller: self, userToken: "self.userToken", appToken: "self.appToken1")
+}
+
